f78a84f1305b5b5e0f46a650fffd446f88dd5b1e
Joseph Emmanuel <jossey1918@gmail.com>
2024-02-02 19:23:06 +0300

    chore: Add mac cleanup script and adjustment in scripts and git configs

---
+++ .scripts/cleanup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+read -p "This script will clear macOS caches and unnecessary files. Continue? (y/n): " confirm
+
+if [ "$confirm" != "y" ]; then
+    echo "Operation canceled."
+    exit 1
+fi
+
+echo "Cleaning system caches..."
+sudo rm -rvf /Library/Caches/*
+sudo rm -rvf /System/Library/Caches/*
+echo "System caches cleared."
+
+echo "Cleaning user caches..."
+rm -rvf ~/Library/Caches/*
+echo "User caches cleared."
+
+echo "Removing logs..."
+sudo rm -rvf /private/var/log/*
+echo "Logs removed."
+
+echo "Deleting temporary files..."
+sudo rm -rvf /private/var/tmp/*
+sudo rm -rfv /private/tmp/*
+echo "Temporary files deleted."
+
+echo "Purging inactive memory..."
+sudo purge
+
+echo "Caches and unnecessary files cleared! 🚀"
